bc3a8fed9f17edc5bbee8f2369be0b56d07b1c17
ngyang94 <ngyangngyang@gmail.com>
2025-11-13 13:14:05 +0800

    add skeleton effect home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,6 +10,7 @@ import {
   CardContent,
 } from "@/components/ui/card";
 import {useSelector,useDispatch} from 'react-redux';
+import { Skeleton } from "@/components/ui/skeleton";
 
 import {type RootState,type AppDispatch} from '../../state/store';
 import {getAnimeList} from '../../state/anime/animeSlice';
@@ -20,13 +21,19 @@ let abortController = new AbortController();
 
 export default function Home(){
 
+    let skeletonAmountToRender: number[] = [];
+    const amountSkeletonTobeRender = 20;
+    for(let i:number=0;i<amountSkeletonTobeRender;i++){
+        skeletonAmountToRender.push(i);
+    }
+
     const animeList = useSelector((state:RootState)=>state.value.animeList);
     const dispatch = useDispatch<AppDispatch>();
     const [animeInput,setAnimeInput] = useState("");
 
     const searchAnimeList = debounce((args)=>{
-
-        dispatch(getAnimeList({animeName:args[0],abortController:abortController}));
+        
+        dispatch(getAnimeList({animeName:args[0]||"",abortController:abortController}));
 
     },1500);
 
@@ -36,7 +43,7 @@ export default function Home(){
             abortController.abort();
             abortController = new AbortController();
         }
-        searchAnimeList(event.target.value);
+        searchAnimeList(event.target?.value.trim());
     }
     useEffect(()=>{
         console.log("render home");
@@ -64,9 +71,29 @@ export default function Home(){
                 </div>
                 <div className="m-5 p-5 grid grid-cols-3 gap-5">
                     {
+                        animeList.length==0&&skeletonAmountToRender.map((skeleton)=>{
+                            return (
+                                <Card className="px-2 py-2" key={skeleton}>
+                                    <CardContent className="p-0">
+                                        
+                                        <div className="grid grid-cols-4 gap-1">
+                                            <Skeleton className="w-20 h-[150px] self-center col-span-1" />
+                                            <div className="px-2 col-span-3 gap-2 flex flex-col space-y-1">
+                                                <Skeleton className="text-1xl h-5 w-[210px]"/>
+                                                <Skeleton className="h-5 w-[210px]"/>
+                                                <Skeleton className="h-5 w-[210px]"/>
+                                                <Skeleton className="h-5 w-[210px]"/>
+                                            </div>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            );
+                        })
+                    }
+                    {
                         animeList.map((anime,index)=>{
                             return (
-                                <Card className="px-2 py-2 group hover:bg-yellow-50">
+                                <Card className="px-2 py-2 group hover:bg-yellow-50" key={index}>
                                     <CardContent className="p-0">
                                         <div className="grid grid-cols-4 gap-1">
                                             <div className="w-1xl h-1xl bg-black overflow-hidden self-center col-span-1">
@@ -78,11 +105,11 @@ export default function Home(){
                                                 <p>Score: {anime.score}</p>
                                                 <p>Popularity: {anime.popularity}</p>
                                                 {index>=12?(
-                                                    <div className="transform -translate-y-full absolute invisible group-hover:visible bg-white p-2 rounded-1xl border-1 before:content-[''] before:block before:border-solid before:border-r-black before:border-r-4 before:border-y-transparent before:border-y-4 before:border-l-0  before:absolute before:transform before:bottom-0 before:-left-1">
+                                                    <div className="transform -translate-y-full absolute invisible group-hover:visible bg-white p-2 rounded-1xl border-2 before:content-[''] before:block before:border-solid before:border-r-black before:border-r-4 before:border-y-transparent before:border-y-4 before:border-l-0  before:absolute before:transform before:bottom-0 before:-left-1">
                                                         {anime.synopsis}
                                                     </div>
                                                 ):(
-                                                    <div className="absolute invisible group-hover:visible bg-white p-2 rounded-1xl border-1 before:content-[''] before:block before:border-solid before:border-r-black before:border-r-4 before:border-y-transparent before:border-y-4 before:border-l-0  before:absolute before:transform before:-top-0.5 before:-left-1">
+                                                    <div className="absolute invisible group-hover:visible bg-white p-2 rounded-1xl border-2 before:content-[''] before:block before:border-solid before:border-r-black before:border-r-4 before:border-y-transparent before:border-y-4 before:border-l-0  before:absolute before:transform before:-top-0.5 before:-left-1">
                                                         {anime.synopsis}
                                                     </div>
                                                 )}
